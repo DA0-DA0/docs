00b6f3259dbb8ba58f83701a77768a0a67aa1bfa
elsehow <yes@cosmopol.is>
2021-12-15 13:58:30 -0800

    url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'DAO DAO',
   tagline: 'DAOs for everyone',
-  url: 'https://DA0-DA0.github.io',
+  url: 'https://docs.daodao.zone',
   organizationName: 'DA0-DA0',
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
