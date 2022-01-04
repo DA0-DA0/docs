446f1c622092cce5ab7420c87eb21698de5244f8
elsehow <yes@cosmopol.is>
2022-01-03 17:20:05 -0800

    meta image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,7 +48,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // meta image https://docusaurus.io/docs/api/themes/configuration#meta-image
-      image: 'static/img/daodao.png',
+      image: 'img/daodao.png',
       // announcement bar
       // https://docusaurus.io/docs/api/themes/configuration#announcement-bar
       announcementBar: {
